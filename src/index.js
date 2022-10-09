2a501cde49e827a9915f910987181dfbbad85b80
antonsoloviev <led_soloviev@mail.ru>
2022-10-09 21:11:48 +0300

    feat (trello-clone): add header

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function clockUpdate() {
+  let clock = document.querySelector('.clock');
+  let date = new Date();
+
+  let hours = date.getHours();
+  if (hours < 10) hours = '0' + hours;
+  clock.children[0].innerHTML = hours;
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) minutes = '0' + minutes;
+  clock.children[1].innerHTML = minutes;
+}
+
+function clockStart() {
+  let clockId = setInterval(clockUpdate, 5000);
+  clockUpdate();
+}
+
+window.addEventListener('load', (event) => {
+  clockStart(); 
+});
